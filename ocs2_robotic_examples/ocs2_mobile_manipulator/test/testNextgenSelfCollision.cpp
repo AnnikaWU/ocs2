f827786878fd5d9166c826f694077496e364eae1
asterich <asterich123@gmail.com>
2026-06-17 14:59:19 +0800

    ocs2_collision_nextgen: fix signs of Jacobian

--- ocs2_robotic_examples/ocs2_mobile_manipulator/test/testNextgenSelfCollision.cpp
+++ ocs2_robotic_examples/ocs2_mobile_manipulator/test/testNextgenSelfCollision.cpp
@@ -29,14 +29,20 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <pinocchio/fwd.hpp>
 
+#include <pinocchio/algorithm/jacobian.hpp>
 #include <pinocchio/algorithm/kinematics.hpp>
+
+#include <cmath>
+#include <utility>
 
 #include <gtest/gtest.h>
 
 #include <ocs2_collision_nextgen/impl/SphereCollisionModel.h>
 #include <ocs2_core/misc/LoadData.h>
 #include <ocs2_core/misc/LoadStdVectorOfPair.h>
+#include <ocs2_pinocchio_interface/urdf.h>
 #include <ocs2_self_collision/PinocchioGeometryInterface.h>
+#include <ocs2_self_collision/SelfCollisionConstraint.h>
 
 #include "ocs2_mobile_manipulator/FactoryFunctions.h"
 #include "ocs2_mobile_manipulator/MobileManipulatorInterface.h"
@@ -47,6 +53,145 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 using namespace ocs2;
 using namespace mobile_manipulator;
+
+namespace {
+
+template <typename SCALAR>
+class IdentityPinocchioMapping final : public PinocchioStateInputMapping<SCALAR> {
+ public:
+  using vector_t = Eigen::Matrix<SCALAR, Eigen::Dynamic, 1>;
+  using matrix_t = Eigen::Matrix<SCALAR, Eigen::Dynamic, Eigen::Dynamic>;
+
+  IdentityPinocchioMapping() = default;
+  ~IdentityPinocchioMapping() override = default;
+  IdentityPinocchioMapping<SCALAR>* clone() const override { return new IdentityPinocchioMapping<SCALAR>(*this); }
+
+  vector_t getPinocchioJointPosition(const vector_t& state) const override { return state; }
+  vector_t getPinocchioJointVelocity(const vector_t& state, const vector_t& input) const override { return input; }
+  std::pair<matrix_t, matrix_t> getOcs2Jacobian(const vector_t& state, const matrix_t& Jq, const matrix_t& Jv) const override {
+    return {Jq, Jv};
+  }
+};
+
+class TestPinocchioPreComputation final : public PreComputation {
+ public:
+  explicit TestPinocchioPreComputation(PinocchioInterface pinocchioInterface) : pinocchioInterface_(std::move(pinocchioInterface)) {}
+
+  TestPinocchioPreComputation* clone() const override { return new TestPinocchioPreComputation(*this); }
+
+  void request(RequestSet request, scalar_t t, const vector_t& x, const vector_t& u) override {
+    pinocchio::forwardKinematics(pinocchioInterface_.getModel(), pinocchioInterface_.getData(), x);
+    pinocchio::updateGlobalPlacements(pinocchioInterface_.getModel(), pinocchioInterface_.getData());
+    pinocchio::computeJointJacobians(pinocchioInterface_.getModel(), pinocchioInterface_.getData(), x);
+  }
+
+  const PinocchioInterface& getPinocchioInterface() const { return pinocchioInterface_; }
+
+ private:
+  PinocchioInterface pinocchioInterface_;
+};
+
+class TestSelfCollisionConstraint final : public SelfCollisionConstraint {
+ public:
+  TestSelfCollisionConstraint(const PinocchioStateInputMapping<scalar_t>& mapping, PinocchioGeometryInterface geometryInterface,
+                              scalar_t minimumDistance)
+      : SelfCollisionConstraint(mapping, std::move(geometryInterface), minimumDistance) {}
+
+  TestSelfCollisionConstraint* clone() const override { return new TestSelfCollisionConstraint(*this); }
+
+ protected:
+  const PinocchioInterface& getPinocchioInterface(const PreComputation& preComputation) const override {
+    return cast<TestPinocchioPreComputation>(preComputation).getPinocchioInterface();
+  }
+};
+
+class TestNextgenSelfCollisionConstraint final : public collision_nextgen::NextgenSelfCollisionConstraint {
+ public:
+  TestNextgenSelfCollisionConstraint(const PinocchioStateInputMapping<scalar_t>& mapping, const pinocchio::Model& model,
+                                     const pinocchio::GeometryModel& geometryModel, scalar_t minimumDistance)
+      : NextgenSelfCollisionConstraint(mapping, model, geometryModel, minimumDistance) {}
+
+  TestNextgenSelfCollisionConstraint* clone() const override { return new TestNextgenSelfCollisionConstraint(*this); }
+
+ protected:
+  const PinocchioInterface& getPinocchioInterface(const PreComputation& preComputation) const override {
+    return cast<TestPinocchioPreComputation>(preComputation).getPinocchioInterface();
+  }
+};
+
+const char* branchSphereUrdf = R"(
+<robot name="branch_spheres">
+  <link name="base"/>
+  <link name="left_link">
+    <collision name="left_sphere">
+      <origin xyz="1.0 0.0 0.0" rpy="0 0 0"/>
+      <geometry><sphere radius="0.4"/></geometry>
+    </collision>
+  </link>
+  <link name="right_link">
+    <collision name="right_sphere">
+      <origin xyz="1.0 0.0 0.0" rpy="0 0 0"/>
+      <geometry><sphere radius="0.4"/></geometry>
+    </collision>
+  </link>
+  <joint name="left_yaw" type="revolute">
+    <parent link="base"/>
+    <child link="left_link"/>
+    <origin xyz="0 0 0" rpy="0 0 0"/>
+    <axis xyz="0 0 1"/>
+    <limit lower="-3.14" upper="3.14" effort="1" velocity="1"/>
+  </joint>
+  <joint name="right_yaw" type="revolute">
+    <parent link="base"/>
+    <child link="right_link"/>
+    <origin xyz="0 0 0" rpy="0 0 0"/>
+    <axis xyz="0 0 1"/>
+    <limit lower="-3.14" upper="3.14" effort="1" velocity="1"/>
+  </joint>
+</robot>
+)";
+
+void compareLinearApproximationForState(const vector_t& state, scalar_t expectedSignedDistanceLowerBound,
+                                        scalar_t expectedSignedDistanceUpperBound) {
+  PinocchioInterface pinocchioInterface = getPinocchioInterfaceFromUrdfString(branchSphereUrdf);
+  PinocchioGeometryInterface geometryInterface(pinocchioInterface, {{"left_link", "right_link"}});
+  TestPinocchioPreComputation preComputation(pinocchioInterface);
+  const vector_t input = vector_t::Zero(0);
+  preComputation.request(Request::SoftConstraint + Request::Approximation, 0.0, state, input);
+
+  const IdentityPinocchioMapping<scalar_t> mapping;
+  TestSelfCollisionConstraint fclConstraint(mapping, PinocchioGeometryInterface(pinocchioInterface, {{"left_link", "right_link"}}), 0.0);
+  TestNextgenSelfCollisionConstraint nextgenConstraint(mapping, pinocchioInterface.getModel(), geometryInterface.getGeometryModel(), 0.0);
+
+  const auto fclLinear = fclConstraint.getLinearApproximation(0.0, state, preComputation);
+  const auto nextgenLinear = nextgenConstraint.getLinearApproximation(0.0, state, preComputation);
+
+  ASSERT_EQ(fclLinear.f.rows(), 1);
+  ASSERT_EQ(nextgenLinear.f.rows(), 1);
+  EXPECT_GT(fclLinear.f[0], expectedSignedDistanceLowerBound);
+  EXPECT_LT(fclLinear.f[0], expectedSignedDistanceUpperBound);
+  EXPECT_TRUE(nextgenLinear.f.isApprox(fclLinear.f, 1e-9)) << "fcl: " << fclLinear.f.transpose()
+                                                          << "\nnextgen: " << nextgenLinear.f.transpose();
+  EXPECT_TRUE(nextgenLinear.dfdx.isApprox(fclLinear.dfdx, 1e-9)) << "fcl:\n"
+                                                                 << fclLinear.dfdx << "\nnextgen:\n"
+                                                                 << nextgenLinear.dfdx;
+}
+
+}  // namespace
+
+TEST(NextgenSelfCollision, HandBuiltSphereLinearApproximationMatchesPinocchioFclWithoutCollision) {
+  vector_t state(2);
+  state << 0.0, M_PI;
+
+  compareLinearApproximationForState(state, 1.0, 1.4);
+}
+
+TEST(NextgenSelfCollision, HandBuiltSphereLinearApproximationMatchesPinocchioFclWithCollision) {
+  vector_t state(2);
+  state << 0.0, 0.25;
+
+  compareLinearApproximationForState(state, -0.7, -0.4);
+}
 
 TEST(NextgenSelfCollision, SphereSoADistancesMatchPinocchioFcl) {
   const std::string taskFile = ocs2::mobile_manipulator::getPath() + "/config/franka/task.info";
